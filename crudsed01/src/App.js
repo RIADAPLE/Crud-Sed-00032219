0a0cd4c3c8eba3094f064aa81e03a8537a724108
RIADAPLE <00032219@uca.edu.sv>
2021-11-20 20:56:07 -0600

    Frontend dinámico

--- crudsed01/src/App.js
+++ crudsed01/src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Table, Button,Container} from 'reactstrap';
@@ -6,7 +6,7 @@ import {Table, Button,Container} from 'reactstrap';
 
 
 function App(){
-  
+  const [inputText, setInputText] = useState("");
   const[usuario,setUsuario] = useState([])
 
   const fetchUsers = async () => {
@@ -19,20 +19,69 @@ function App(){
     } catch (error) {
       console.error(error);
     }
-  }
-  const addToList = () => {
-    console.log(usuario);
-  }
+  };
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  const onInputChange = (e) => {
+    setInputText(e.target.value);
+  };
+
+  const onUserSubmit = async () => {
+    const url = `${process.env.REACT_APP_API_URL}/users`;
+    const conf = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ username: inputText}),
+    };
+
+    try {
+      const response = await fetch(url, conf);
+      const obj = await response.json();
+
+      if (obj.status === "success") {
+        await fetchUsers();
+        setInputText("");        
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const onUserDelete = async (id) => {
+    const url = `${process.env.REACT_APP_API_URL}/users/${id}`;
+    const conf = {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+
+    try {
+      const response = await fetch(url, conf);
+      const obj = await response.json();
+
+      if (obj.status === "success") {
+        await fetchUsers();
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <>
     <Container>
       <br/>
       <label>Usuario</label>{" "}
-      <input type="text" onChange={(event) =>{
-        setUsuario(event.target.value)}}/>{" "}
+      <input type="text" value={inputText} onChange={onInputChange}/>{" "}
       
 
-    <Button color="primary" onClick={addToList}>Insertar Nuevo Usuario</Button>
+    <Button color="primary" onClick={onUserSubmit}>Insertar Nuevo Usuario</Button>
     <br/><br/>
 
       <Table>
@@ -40,13 +89,14 @@ function App(){
       <th>Roles</th>
       <th>Botones</th></tr></thead>
       <tbody>
-      
-          <tr>
-            <td>{"id"}</td>
-            <td>{"usuario"}</td>
-            <td><Button color="primary">Editar</Button>{"\t\t\t\t"}
-            <Button color="danger">Eliminar</Button></td>
-            </tr>
+      {usuario && usuario.map(({_id, usuario}) =>(
+        <tr>
+        <td key={_id}>{_id}</td>
+        <td>{usuario}</td>
+        <td><Button color="primary">Editar</Button>{"\t\t\t\t"}
+        <Button color="danger" onClick={()=> onUserDelete(_id)}>Eliminar</Button></td>
+        </tr>
+      ))}    
         
       </tbody>
 
